aaa959ef242fb97a9be227f0c3d501d9c350c4c6
jcvisk <juan.carlos.gcn@gmail.com>
2022-12-09 00:52:07 -0600

    configurando linechart

--- app/src/main/java/com/iunis/arrhythmiapp/presentation/chart/HeartDataChartFragment.kt
+++ app/src/main/java/com/iunis/arrhythmiapp/presentation/chart/HeartDataChartFragment.kt
@@ -5,17 +5,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.data.BarData
-import com.github.mikephil.charting.data.BarDataSet
-import com.github.mikephil.charting.data.BarEntry
-import com.github.mikephil.charting.data.DataSet
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.utils.ColorTemplate
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.firestore.Query
+import com.iunis.arrhythmiapp.data.utils.FirebaseConstants.HEART_DATA_COLLECTION
 import com.iunis.arrhythmiapp.databinding.FragmentHeartDataChartBinding
+import com.iunis.arrhythmiapp.domain.model.HeartData
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -25,6 +27,8 @@ class HeartDataChartFragment : Fragment() {
 
     private var _binding: FragmentHeartDataChartBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var firestoreListener: ListenerRegistration
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,44 +42,55 @@ class HeartDataChartFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        lineChart = binding.lChart
-
-        val list:ArrayList<Entry> = ArrayList()
-        list.add(Entry(100f, 100f))
-        list.add(Entry(101f, 101f))
-        list.add(Entry(102f, 101.5f))
-        list.add(Entry(103f, 103f))
-        list.add(Entry(104f, 102.5f))
-
-        val lineDataSet = LineDataSet(list,"Sistolica")
-
-        lineDataSet.setColors(ColorTemplate.MATERIAL_COLORS, 255)
-
-        lineDataSet.valueTextColor=Color.BLACK
-
-
-        val list2:ArrayList<Entry> = ArrayList()
-        list2.add(Entry(100f, 100f))
-        list2.add(Entry(101f, 102f))
-        list2.add(Entry(102f, 101f))
-        list2.add(Entry(103f, 103.5f))
-        list2.add(Entry(104f, 102f))
-
-        val lineDataSet2 = LineDataSet(list2,"diastolica")
-
-        lineDataSet2.setColors(ColorTemplate.MATERIAL_COLORS, 255)
-
-        lineDataSet2.valueTextColor=Color.BLACK
-
-
-
-
-        val lineData = LineData(lineDataSet, lineDataSet2)
-
-        lineChart.data=lineData
-        lineChart.description.text="Line Chart"
-        lineChart.animateY(2000)
+        configFirestore()
 
     }
 
+
+    private fun configFirestore(){
+        val db = FirebaseFirestore.getInstance()
+        db.collection(HEART_DATA_COLLECTION).orderBy("fecha", Query.Direction.DESCENDING).limit(10)
+            .get()
+            .addOnSuccessListener { snapshots ->
+
+                lineChart = binding.lChart
+
+                val systolicList:ArrayList<Entry> = ArrayList()
+                val diastolicList:ArrayList<Entry> = ArrayList()
+                val pulseList:ArrayList<Entry> = ArrayList()
+
+                var distancia = 1f
+                for(document in snapshots){
+                    val heartData = document.toObject(HeartData::class.java)
+                    systolicList.add(Entry(distancia++, heartData.systolic!!.toFloat()))
+                    diastolicList.add(Entry(distancia++, heartData.diastolic!!.toFloat()))
+                    pulseList.add(Entry(distancia++, heartData.pulse!!.toFloat()))
+                }
+
+                val systolicLineDataSet = LineDataSet(systolicList,"Sistólica")
+                systolicLineDataSet.setColors(ColorTemplate.MATERIAL_COLORS, 255)
+                systolicLineDataSet.valueTextColor=Color.BLACK
+
+                val diastolicLineDataSet = LineDataSet(diastolicList,"Diastólica")
+                diastolicLineDataSet.setColors(ColorTemplate.MATERIAL_COLORS, 255)
+                diastolicLineDataSet.valueTextColor=Color.BLACK
+
+                val pulseLineDataSet = LineDataSet(pulseList,"Pulso")
+                pulseLineDataSet.setColors(ColorTemplate.MATERIAL_COLORS, 255)
+                pulseLineDataSet.valueTextColor=Color.BLACK
+
+
+
+
+                val lineData = LineData(systolicLineDataSet, diastolicLineDataSet, pulseLineDataSet)
+
+                lineChart.data=lineData
+                lineChart.description.text="Line Chart"
+                lineChart.animateY(2000)
+
+            }
+            .addOnFailureListener{
+                Toast.makeText(requireContext(),"Error al consultar datos", Toast.LENGTH_SHORT).show()
+            }
+    }
 }
